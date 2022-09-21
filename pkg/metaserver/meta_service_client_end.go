905b21a62cece10143a018427426b4ca75d1669c
CrazyCollin <lazycollinl@gmail.com>
2022-09-22 01:14:06 +0800

    chore(meta-server):Build meta server connection service client end

--- pkg/metaserver/meta_service_client_end.go
+++ pkg/metaserver/meta_service_client_end.go
@@ -2,12 +2,35 @@ package metaserver
 
 import (
 	"google.golang.org/grpc"
+	"rstorage/pkg/log"
 	pb "rstorage/pkg/protocol"
 )
 
 type MetaServiceClientEnd struct {
-	serviceCli *pb.MetaServiceClient
 	id         uint64
 	addr       string
+	serviceCli *pb.MetaServiceClient
 	conns      []*grpc.ClientConn
+}
+
+func NewMetaServiceClientEnd(id uint64, addr string) *MetaServiceClientEnd {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Log.Errorf("err: %v ", err.Error())
+	}
+	var conns []*grpc.ClientConn
+	conns = append(conns, conn)
+	rpcClient := pb.NewMetaServiceClient(conn)
+	return &MetaServiceClientEnd{
+		id:         id,
+		addr:       addr,
+		serviceCli: &rpcClient,
+		conns:      conns,
+	}
+}
+
+func (e *MetaServiceClientEnd) CloseClientConns() {
+	for _, conn := range e.conns {
+		_ = conn.Close()
+	}
 }
